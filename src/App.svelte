a21d87e1778f53af65517dc43b63e981aeda56dd
Ludvík Prokopec <prokolud@fit.cvut.cz>
2023-02-17 12:39:57 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Route, Router } from 'svelte-routing'
+
+	import Index from './routes/index.svelte'
+</script>
+
+<Router>
+	<Route path="/" component={Index} />
+</Router>
